473aa10c9cbebe3f1a005b7dc443902340ac60e6
rastogi102 <nikhilrastogi102@gmail.com>
2023-09-04 15:02:12 +0530

    initial commit

---
+++ WebAPIMovie/WebAPIMovie/Data/MovieDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAPIMovie.Model;
+
+namespace WebAPIMovie.Data
+{
+    public class MovieDbContext : DbContext
+    {
+        public MovieDbContext (DbContextOptions<MovieDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAPIMovie.Model.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ WebAPIMovie/WebAPIMovie/Model/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace WebAPIMovie.Model
+{
+    [Table("Movie")]
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public double Rating { get; set; }
+
+    }
+}
